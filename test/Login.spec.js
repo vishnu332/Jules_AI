1d689c750be5354d347c4119a341bcea36716eba
Vishnu <vishnu332@gmail.com>
2024-08-04 14:01:16 +0530

    Added 3 cases for login page

--- test/Login.spec.js
+++ test/Login.spec.js
@@ -1,5 +1,6 @@
 const { test, expect } = require("@playwright/test");
 const { chromium } = require("playwright");
+const locators = require("../locators/loginLocators.json")
 
 test("Verify Login", async () => {
   const browser = await chromium.launch();
@@ -13,8 +14,29 @@ test("Verify Login", async () => {
   // Navigate to website
   await page.goto("https://demo.haroldwaste.com/");
 
-  await page.locator('[type="email"]').fill('qa@julesai.com');
-  await page.locator('[type="password"]').fill('QaJULES2023!');
-  await page.locator('[type="submit"]').click();
-  await expect(page.locator('[data-test-id="header-menu"]')).toBeVisible();
+  // Case 1: Password too short error mesage displayed
+  await page.locator(locators.emailInput).fill('qa@julesai.com');
+  await page.locator(locators.passwordInput).fill('QaJU');
+  await page.locator(locators.signInButon).click();
+  await expect(page.locator(locators.shortPasswordError)).toBeVisible();
+
+  // Case 2: Enter wrong password and verify alert message. 
+  // Bug: The close button on alert message does not close the alert.
+  await page.locator(locators.emailInput).fill('qa@julesai.com');
+  await page.locator(locators.passwordInput).fill('QaJULES2023');
+  await page.locator(locators.signInButon).click();
+  await expect(page.locator(locators.alertMessage)).toBeVisible();
+
+  try {
+    await page.locator(locators.alertMessageClose).click({ timeout: 2000 })
+    await expect(page.locator(locators.alertMessage)).not.toBeVisible();
+  } catch (error) {
+    console.log("Bug: 'X' button on the alert message does not close the alert.");
+  }
+
+  // Case 3: Login with correct username and password and validate
+  await page.locator(locators.emailInput).fill('qa@julesai.com');
+  await page.locator(locators.passwordInput).fill('QaJULES2023!');
+  await page.locator(locators.signInButon).click();
+  await expect(page.locator(locators.headerMenu)).toBeVisible();
 });
